079a973bb778621ad9289057df5436306a036ef5
camillleweins <camilleweins@gmail.com>
2017-05-11 22:57:43 -0400

    sound-off
    working sound, button and led
    -not integrated with server code

---
+++ new_button_neopixel_code_sound.ino
@@ -0,0 +1,81 @@
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_VS1053.h>
+#include <Adafruit_NeoPixel.h>
+
+#define VS1053_RESET   -1     // VS1053 reset pin (not used!)
+
+#define BUTTON_PIN 4
+#define PIXEL_PIN 5
+#define PIXEL_COUNT 1
+
+#define VS1053_CS      16     // VS1053 chip select pin (output)
+#define VS1053_DCS     15     // VS1053 Data/command select pin (output)
+#define CARDCS          2     // Card chip select pin
+#define VS1053_DREQ     0     // VS1053 Data request, ideally an Interrupt pin
+
+char mychar;
+
+Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+bool oldState = HIGH;
+int showType = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("\n\nAdafruit VS1053 Feather Test");
+  musicPlayer.setVolume(10,10);
+  
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+//  musicPlayer.sineTest(0x44, 500);
+
+//  musicPlayer.playFullFile("track001.mp3");
+}
+
+void loop() {
+  // Get current button state.
+  bool newState = digitalRead(BUTTON_PIN);
+
+  // Check if state changed from high to low (button press).
+  if (newState == LOW && oldState == HIGH) {
+    // Short delay to debounce button.
+    delay(5);
+    // Check if button is still low after debounce.
+    newState = digitalRead(BUTTON_PIN);
+    if (newState == HIGH) {
+      musicPlayer.stopPlaying();
+//      musicPlayer.sineTest(0x44, 500);
+      colorWipe(strip.Color(0, 255, 0), 50);
+    } else {
+      colorWipe(strip.Color(0, 0, 0), 50);
+    }
+  }
+  // Set the last button state to the old state.
+  oldState = newState;
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
